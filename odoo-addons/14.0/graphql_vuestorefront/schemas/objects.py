ced08a13dd2f0860f43835ff2d5fa44b6bd547e1
Carlos Fonseca <cfonseca@odoogap.com>
2021-12-29 14:08:47 +0000

    get public categories from product based on current website

--- odoo-addons/14.0/graphql_vuestorefront/schemas/objects.py
+++ odoo-addons/14.0/graphql_vuestorefront/schemas/objects.py
@@ -361,6 +361,9 @@ class Product(OdooObjectType):
         return None
 
     def resolve_categories(self, info):
+        website = self.env['website'].get_current_website()
+        if website:
+            return self.public_categ_ids.filtered(lambda c: c.website_id and c.website_id.id == website.id) or None
         return self.public_categ_ids or None
 
     def resolve_available_threshold(self, info):
