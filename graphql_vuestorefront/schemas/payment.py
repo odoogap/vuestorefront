1531b12f917a9446a66b13fe8fc0669c3a31b5dc
Carlos Fonseca <cfonseca@odoogap.com>
2022-05-31 20:55:16 +0100

    [FIX] resolve payment confirmation

--- graphql_vuestorefront/schemas/payment.py
+++ graphql_vuestorefront/schemas/payment.py
@@ -62,17 +62,14 @@ class PaymentQuery(graphene.ObjectType):
         payment_transaction_id = request.session.get('__payment_tx_ids__')[0]
 
         if payment_transaction_id:
-
-            payment_transaction = PaymentTransaction.sudo().search([
-                ('id', '=', payment_transaction_id)], limit=1)
-
+            payment_transaction = PaymentTransaction.sudo().search([('id', '=', payment_transaction_id)], limit=1)
             sale_order_id = payment_transaction.sale_order_ids.ids[0]
 
-        if sale_order_id:
-            order = Order.sudo().search([('id', '=', sale_order_id)], limit=1)
+            if sale_order_id:
+                order = Order.sudo().search([('id', '=', sale_order_id)], limit=1)
 
-            if order.exists():
-                return CartData(order=order)
+                if order.exists():
+                    return CartData(order=order)
 
         raise GraphQLError(_('Cart does not exist'))
 
