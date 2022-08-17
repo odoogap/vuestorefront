30896de8446eb5d38a588564aca683d259b682b3
Carlos Fonseca <cfonseca@odoogap.com>
2022-08-17 12:53:51 +0100

    [ADD] vsf categories, products and redirects custom routes generator for vsf

--- graphql_vuestorefront/controllers/main.py
+++ graphql_vuestorefront/controllers/main.py
@@ -2,6 +2,7 @@
 # Copyright 2021 ODOOGAP/PROMPTEQUATION LDA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import os
 import json
 import werkzeug
 from odoo import http
@@ -9,24 +10,9 @@ from odoo.addons.graphql_base import GraphQLControllerMixin
 from odoo.addons.payment.controllers.portal import PaymentProcessing
 from odoo.addons.payment_adyen.controllers.main import AdyenController
 from odoo.http import request, Response
+from urllib.parse import urlparse
 
 from ..schema import schema
-
-
-class VSFWebsite(http.Controller):
-
-    @http.route('/vsf/redirects', type='http', auth='public', csrf=False)
-    def vsf_redirects(self):
-        redirects_list = []
-        redirects = request.env['website.rewrite'].sudo().search([])
-        if redirects:
-            for redirect in redirects:
-                redirect_dict = {'from': redirect.url_from, 'to': redirect.url_to}
-                redirects_list.append(redirect_dict)
-        result = json.dumps(redirects_list)
-        return Response(result, headers={
-            'Content-Type': 'application/json',
-        })
 
 
 class VSFAdyenController(AdyenController):
@@ -73,14 +59,11 @@ class VSFAdyenController(AdyenController):
 
 class GraphQLController(http.Controller, GraphQLControllerMixin):
 
-    def get_domain_of_request_host(self):
-        """ Trying get the http_request_host, to update the language that will be used """
+    def _set_website_context(self):
+        """Set website context based on http_request_host header."""
         try:
             request_host = request.httprequest.headers.environ['HTTP_RESQUEST_HOST']
-
-            domain = 'https://%s' % request_host
-
-            website = request.env['website'].search([('domain', '=', domain)], limit=1)
+            website = request.env['website'].search([('domain', 'ilike', request_host)], limit=1)
             if website:
                 context = dict(request.context)
                 context.update({
@@ -92,8 +75,8 @@ class GraphQLController(http.Controller, GraphQLControllerMixin):
                 request_uid = http.request.env.uid
                 website_uid = website.sudo().user_id.id
 
-                if request_uid != website_uid and \
-                        request.env['res.users'].sudo().browse(request_uid).has_group('base.group_public'):
+                if request_uid != website_uid \
+                        and request.env['res.users'].sudo().browse(request_uid).has_group('base.group_public'):
                     request.uid = website_uid
         except:
             pass
@@ -101,7 +84,7 @@ class GraphQLController(http.Controller, GraphQLControllerMixin):
     # The GraphiQL route, providing an IDE for developers
     @http.route("/graphiql/vsf", auth="user")
     def graphiql(self, **kwargs):
-        self.get_domain_of_request_host()
+        self._set_website_context()
         return self._handle_graphiql_request(schema.graphql_schema)
 
     # Optional monkey patch, needed to accept application/json GraphQL
@@ -115,5 +98,68 @@ class GraphQLController(http.Controller, GraphQLControllerMixin):
     # (such as origin restrictions) to this route.
     @http.route("/graphql/vsf", auth="public", csrf=False)
     def graphql(self, **kwargs):
-        self.get_domain_of_request_host()
+        self._set_website_context()
         return self._handle_graphql_request(schema.graphql_schema)
+
+    @http.route('/vsf/categories', type='http', auth='public', csrf=False)
+    def vsf_categories(self):
+        self._set_website_context()
+        website = request.env['website'].get_current_website()
+
+        categories = []
+
+        if website.default_lang_id:
+            lang_code = website.default_lang_id.code
+            domain = [('website_slug', '!=', False)]
+
+            for category in request.env['product.public.category'].sudo().search(domain):
+                category = category.with_context(lang=lang_code)
+                categories.append(category.website_slug)
+
+        return Response(
+            json.dumps(categories),
+            headers={'Content-Type': 'application/json'},
+        )
+
+    @http.route('/vsf/products', type='http', auth='public', csrf=False)
+    def vsf_products(self):
+        self._set_website_context()
+        website = request.env['website'].get_current_website()
+
+        products = []
+
+        if website.default_lang_id:
+            lang_code = website.default_lang_id.code
+            domain = [('website_published', '=', True), ('website_slug', '!=', False)]
+
+            for product in request.env['product.template'].sudo().search(domain):
+                product = product.with_context(lang=lang_code)
+
+                url_parsed = urlparse(product.website_slug)
+                name = os.path.basename(url_parsed.path)
+                path = product.website_slug.replace(name, '')
+
+                products.append({
+                    'name': name,
+                    'path': '{}:slug'.format(path),
+                })
+
+        return Response(
+            json.dumps(products),
+            headers={'Content-Type': 'application/json'},
+        )
+
+    @http.route('/vsf/redirects', type='http', auth='public', csrf=False)
+    def vsf_redirects(self):
+        redirects = []
+
+        for redirect in request.env['website.rewrite'].sudo().search([]):
+            redirects.append({
+                'from': redirect.url_from,
+                'to': redirect.url_to,
+            })
+
+        return Response(
+            json.dumps(redirects),
+            headers={'Content-Type': 'application/json'},
+        )
