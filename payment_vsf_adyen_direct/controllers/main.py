725169cb669ce2a36c29ab2374b07fdce0c58207
Bruno Figueiredo <bfigueiredo@odoogap.com>
2022-09-20 00:42:45 +0100

    [FIX] Fixing the payment_sucess_return_url when the payment is done On the Portal side with success

--- payment_vsf_adyen_direct/controllers/main.py
+++ payment_vsf_adyen_direct/controllers/main.py
@@ -215,8 +215,8 @@ class AdyenDirectController(http.Controller):
         vsf_payment_error_return_url = website.vsf_payment_error_return_url
         # Redirects to VSF -> Pay by Link (On Portal)
         if payment_transaction.vsf_pay_by_link:
-            vsf_payment_success_return_url = website.vsf_pay_url
-            vsf_payment_error_return_url = sale_order.get_portal_url()
+            vsf_payment_success_return_url = sale_order.get_portal_url()
+            vsf_payment_error_return_url = website.vsf_pay_url
 
         request.session["__payment_tx_ids__"] = [payment_transaction.id]
 
@@ -313,7 +313,7 @@ class AdyenDirectController(http.Controller):
                     vsf_payment_success_return_url = website.vsf_payment_success_return_url
                     # Redirects to VSF -> Pay by Link (On Portal)
                     if payment_transaction.vsf_pay_by_link:
-                        vsf_payment_success_return_url = website.vsf_pay_url
+                        vsf_payment_success_return_url = sale_order.get_portal_url()
 
                     request.session["__payment_tx_ids__"] = [payment_transaction.id]
 
