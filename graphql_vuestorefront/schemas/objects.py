2d1f045cfc00ec1a5a1ddb8d0e9434627d57b511
Carlos Fonseca <cfonseca@odoogap.com>
2023-06-09 09:54:30 +0100

    [IMP] change first variant type on product object

--- graphql_vuestorefront/schemas/objects.py
+++ graphql_vuestorefront/schemas/objects.py
@@ -334,7 +334,7 @@ class Product(OdooObjectType):
     attribute_values = graphene.List(graphene.NonNull(lambda: AttributeValue),
                                      description='Specific to Product Template')
     product_variants = graphene.List(graphene.NonNull(lambda: Product), description='Specific to Product Template')
-    first_variant = graphene.Int(description='Specific to use in Product Template')
+    first_variant = graphene.Field(lambda: Product, description='Specific to use in Product Template')
     json_ld = generic.GenericScalar()
 
     def resolve_type_id(self, info):
