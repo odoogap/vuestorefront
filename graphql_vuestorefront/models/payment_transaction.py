0309e57a4e9c474875e2926ebbf0781ed23401a8
Bruno Figueiredo <bfigueiredo@odoogap.com>
2024-03-05 17:26:46 +0000

    [IMP] Updating the copyrights to 2024

--- graphql_vuestorefront/models/payment_transaction.py
+++ graphql_vuestorefront/models/payment_transaction.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2023 ERPGAP/PROMPTEQUATION LDA
+# Copyright 2024 ERPGAP/PROMPTEQUATION LDA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import models, api, fields, tools, _
