4ad8246cb334c6169b188d4128995320af665fb1
Ajay Javiya <ajaviya@odoogap.com>
2024-03-21 18:34:04 +0530

    [FIX] image resize not working

--- graphql_vuestorefront/models/__init__.py
+++ graphql_vuestorefront/models/__init__.py
@@ -3,9 +3,9 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from . import invalidate_cache
-from . import ir_http
 from . import website
 from . import product
 from . import res_config_settings
 from . import res_users
 from . import payment_transaction
+from . import ir_binary
