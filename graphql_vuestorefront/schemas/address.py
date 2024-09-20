821bd24593dc7e222422cba74beb7b48b1a2dd08
Ajay Javiya <ajaviya@odoogap.com>
2024-09-20 19:36:41 +0530

    [IMP] prevent update or delete of public user address

--- graphql_vuestorefront/schemas/address.py
+++ graphql_vuestorefront/schemas/address.py
@@ -202,6 +202,10 @@ class UpdateAddress(graphene.Mutation):
         request.website = website
         order = website.sale_get_order()
 
+        websites = env['website'].sudo().search([])
+        if address['id'] in websites.mapped('user_id.partner_id.id'):
+            raise GraphQLError(_("Can't update public user address."))
+
         partner = get_partner(env, address['id'], order, website)
 
         values = {}
@@ -247,6 +251,10 @@ class DeleteAddress(graphene.Mutation):
         website = env['website'].get_current_website()
         request.website = website
         order = website.sale_get_order()
+
+        websites = env['website'].sudo().search([])
+        if address['id'] in websites.mapped('user_id.partner_id.id'):
+            raise GraphQLError(_("Can't update public user address."))
 
         partner = get_partner(env, address['id'], order, website)
 
