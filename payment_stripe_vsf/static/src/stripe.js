f5b4b18c7914744a59cca08c3d5322cbe52ddd2f
Bruno Figueiredo <bfigueiredo@odoogap.com>
2024-10-17 18:41:12 +0100

    [IMP] Adding one condition to be possible to test the PayPal Payment Method on the "payment_stripe_vsf" Module

--- payment_stripe_vsf/static/src/stripe.js
+++ payment_stripe_vsf/static/src/stripe.js
@@ -11,32 +11,32 @@ publicWidget.registry.WebsiteSaleStripe = publicWidget.Widget.extend({
     init: function () {
         let self = this;
         this._super.apply(this, arguments);
-        this.stripeJs = Stripe('pk_test_51Q5ULNFMmqSmtPHODi8Nwo25z2hnU5TJ2r76oWuYeJwVSrjNlThcieFc5Nmt46cw1kLrcRMDizyzgZE7vUOVlMih00y4iZJn5U', {
+        this.stripeJs = Stripe('pk_test_51Q5TQCCsRfUCvfWIlYOylZI5rSJ0FoqZGBFSFQ9rdDGmr6ZXTjKk038fWG7YPCDHzVlQsP5fAPp7Frxs2bFMVacC00CSEYMmw3', {
             'apiVersion': '2019-05-16',  // The API version of Stripe implemented in this module.
         });
-        let clientSecret = 'pi_3QAwbeFMmqSmtPHO0HP0pDxO_secret_mKWy2T2iZoWfp0aX5Z6w4ezCo';
+        let clientSecret = 'pi_3QAxLoCsRfUCvfWI1BsCKtp1_secret_ik0R8Ez0lvJJcQq1wpl93cCyt';
         this.clientSecret = clientSecret;
         this.billingDetails = {
-            "name": "Test02 Affirm",
-            "email": "test02a@hotmail.com",
-            "phone": "8436726176",
+            "name": "Test02",
+            "email": "test02@hotmail.com",
+            "phone": "966222333",
             "address": {
-                "line1": "1883 Broadway Street",
+                "line1": "Rua Nossa Senhora de Fatima",
                 "line2": "",
-                "city": "Pageland",
-                "state": "NC",
-                "country": "US",
-                "postal_code": "29728"
+                "city": "Viseu",
+                "state": "18",
+                "country": "PT",
+                "postal_code": "3510-605"
             }
         }
-        this.return_url = `http://2f2e-2001-818-de5d-da00-3f59-eac8-191a-c707.ngrok-free.app/payment/stripe/return?reference=S00093`;
+        this.return_url = `http://2f2e-2001-818-de5d-da00-3f59-eac8-191a-c707.ngrok-free.app/payment/stripe/return?reference=S00070-1`;
         let elementsOptions =  {
             appearance: { theme: 'stripe' },
             currency: "usd",
             captureMethod: "automatic",
             mode: 'payment',
-            amount:37000,
-            paymentMethodTypes: ['card', 'affirm', 'klarna']
+            amount:7381,
+            paymentMethodTypes: ['card', 'klarna', 'paypal']
         };
         this.paymentType = '';
         const paymentElementOptions = {
@@ -68,6 +68,15 @@ publicWidget.registry.WebsiteSaleStripe = publicWidget.Widget.extend({
             });
         } else if (this.paymentType === 'klarna') {
             return this.stripeJs.confirmKlarnaPayment(this.clientSecret,
+                {
+                    payment_method: {
+                    billing_details: self.billingDetails
+                },
+                // Return URL where the customer should be redirected after the authorization.
+                return_url: self.return_url
+            });
+        } else if (this.paymentType === 'paypal') {
+            return this.stripeJs.confirmPayPalPayment(this.clientSecret,
                 {
                     payment_method: {
                     billing_details: self.billingDetails
