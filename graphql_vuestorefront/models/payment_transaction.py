2b8d08e66bd7acd8a7e067f071ffefe765cf5585
Bruno Figueiredo <bfigueiredo@odoogap.com>
2024-10-21 12:54:54 +0100

    [IMP] Changing the _set_pending() function on the payment_provider, to prevent the creation of a new Cart, when the Transaction is in one pending state

--- graphql_vuestorefront/models/payment_transaction.py
+++ graphql_vuestorefront/models/payment_transaction.py
@@ -9,3 +9,21 @@ class PaymentTransactionInherit(models.Model):
     _inherit = 'payment.transaction'
 
     created_on_vsf = fields.Boolean(string='Created on Vsf?', default=False)
+
+    def _set_pending(self, state_message=None):
+        """
+            Overwrite steps:
+
+            1- Not update the Transaction to "Pending" ;
+            2- Not update the SO to 'sent' State;
+            3- Not send the quotation email confirmation;
+            4- Not Log the message on the SO chatter;
+        """
+        for record in self:
+            if not record.created_on_vsf:
+                super(PaymentTransactionInherit, record)._set_pending(state_message=state_message)
+            else:
+                allowed_states = ('draft',)
+                target_state = 'draft'
+                # txs_to_process
+                self._update_state(allowed_states, target_state, state_message=None)
