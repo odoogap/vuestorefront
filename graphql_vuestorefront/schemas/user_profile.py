080d1730cdd190aa378262954fe1734f3f086f27
Ajay Javiya <ajaviya@odoogap.com>
2025-01-20 19:21:59 +0530

    [ADD] mutation to delete user profile/account

--- graphql_vuestorefront/schemas/user_profile.py
+++ graphql_vuestorefront/schemas/user_profile.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2022 ODOOGAP/PROMPTEQUATION LDA
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
 import graphene
+
 from graphql import GraphQLError
-from odoo import _
+from odoo import _, SUPERUSER_ID
 from odoo.http import request
 
 from odoo.addons.graphql_vuestorefront.schemas.objects import Partner
@@ -62,5 +62,42 @@ class UpdateMyAccount(graphene.Mutation):
         return partner
 
 
+class DeleteMyAccount(graphene.Mutation):
+
+    Output = graphene.Boolean
+
+    @staticmethod
+    def mutate(self, info):
+        env = info.context["env"]
+        website = env['website'].get_current_website()
+        request.website = website
+        user = request.env.user.sudo()
+        website_user = website.user_id
+
+        # Prevent "Public User" to be deleted
+        if user.id == website_user.id or not user.has_group('base.group_portal'):
+            raise GraphQLError(_('User cannot be deleted.'))
+
+        partner = user.partner_id
+        if partner:
+            if not partner.email:
+                raise GraphQLError(_(f"Cannot send email: user {user.name} has no email address."))
+            template = env.ref('graphql_vuestorefront.delete_my_account_email').sudo()
+            template.send_mail(user.id, raise_exception=True, force_send=True)
+            request.session.logout()
+            user.api_key_ids._remove()
+
+            try:
+                # A user can not self-deactivate
+                user.with_user(SUPERUSER_ID).action_archive()
+            except Exception:
+                pass
+            user.with_user(SUPERUSER_ID).unlink()
+        else:
+            raise GraphQLError(_('User does not exist.'))
+        return True
+
+
 class UserProfileMutation(graphene.ObjectType):
     update_my_account = UpdateMyAccount.Field(description='Update MyAccount')
+    delete_my_account = DeleteMyAccount.Field(description='Delete MyAccount')
